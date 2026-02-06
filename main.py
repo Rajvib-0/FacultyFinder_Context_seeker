19d493a65ae3d111a3520c3194c375fbf31c16e1
Rajvib-0 <rbmoriarty429@gmail.com>
2026-02-06 11:53:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ import sqlite3
 
 app = FastAPI()
 
+@app.get("/")
+def health_check():
+    return {"status": "ok", "service": "Faculty Finder API"}
+
 @app.get("/faculty")
 def get_faculty_data():
     conn = sqlite3.connect("faculty.db")
     cursor = conn.cursor()
-
     cursor.execute("SELECT * FROM faculty")
     columns = [col[0] for col in cursor.description]
     rows = cursor.fetchall()
     conn.close()
-
-    # Convert rows to list of dicts
     faculty_list = [dict(zip(columns, row)) for row in rows]
     return {"faculty": faculty_list}
